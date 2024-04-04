5d6500d91cc4d9c6fa96439c01650ac311f3b8e7
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2024-04-04 16:57:18 +0700

    listchat

--- screens/afterLogin/listChat/ListChat.js
+++ screens/afterLogin/listChat/ListChat.js
@@ -1,72 +1,45 @@
-import { View, Text, TouchableOpacity, Image, FlatList, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, Dimensions, Image, FlatList, SafeAreaView } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { FontAwesome, AntDesign } from '@expo/vector-icons';
 import { TextInput } from 'react-native-paper';
 // import store from '../../../Redux/Redux';
 import { useSelector } from 'react-redux';
+import { func } from 'prop-types';
 const ListChat = ({ navigation, route }) => {
-  // useEffect(() => {
-  //   navigation.setOptions({
-  //     headerRight: () => (
-  //       <TouchableOpacity onPress={() => navigation.replace('NewScreen')}>
-  //         <Text>Replace</Text>
-  //       </TouchableOpacity>
-  //     ),
-  //   });
-  // }, [navigation]);
   const name = useSelector((state) => state.account.userName);
   const avt = useSelector((state) => state.account.avt);
-
+  const { width } = Dimensions.get('window');
   const id = useSelector((state) => state.account.id);
 
   const obj = useSelector((state) => state.account);
   console.log(obj);
-  // const obj = {
-  //   id: "RGpCgF0lR1aGVcttckhAbBHWcSp2",
-  //   userName: "Nguyễn Thanh Sơn",
-  //   phone: "84387866829",
-  //   gender: "Nam",
-  //   avt: "https://projectappchat.blob.core.windows.net/containerapp/dadeb8a7-fae6-45fc-ab34-40588bcb0c40.jpeg",
-  //   coverImage: null,
-  //   logOut: null,
-  //   conversation: [
-  //     {
-  //       updateLast: "2024-04-03T08:28:54.222",
-  //       conversationType: "single",
-  //       user: {
-  //         id: "jgfqCBTFdEgDmpHHXaNHdZV8B982"
-  //       },
-  //       lastMessage: {
-  //         id: "ccd4fb61-8c44-43aa-b72c-5ca04532d4ab",
-  //         messageType: "Text",
-  //         senderDate: "2024-04-03T08:28:54.062",
-  //         sender: {
-  //           id: "RGpCgF0lR1aGVcttckhAbBHWcSp2"
-  //         },
-  //         receiver: {
-  //           id: "jgfqCBTFdEgDmpHHXaNHdZV8B982"
-  //         },
-  //         react: null,
-  //         seen: null,
-  //         replyMessage: null,
-  //         content: "Ha🤣ha"
-  //       }
-  //     }
-  //   ],
-  //   friendList: [
 
-  //   ],
-  //   bio: null,
-  //   dob: null
-  // }
+  const calcTime = (time) => {
+    const date = new Date(time)
+    date.setUTCHours(date.getUTCHours() + 7)
+    const now = new Date()
+    const diff = now - date
+    const seconds = diff / 1000
+    if(seconds < 60) return 'Vừa xong'
+    const minutes = seconds / 60
+    if(minutes < 60) return `${Math.floor(minutes)} phút`
+    const hours = minutes / 60
+    if(hours < 24) return `${Math.floor(hours)} giờ`
+    const days = hours / 24
+    if(days < 7) return `${Math.floor(days)} ngày`
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <View style={{
-        backgroundColor: 'cyan', paddingHorizontal: 10,
+        backgroundColor: 'cyan',
         height: 50, flexDirection: 'row',
         width: '100%', justifyContent: 'space-between', alignItems: 'center'
       }}>
-        <FontAwesome name="search" size={35} color="white" />
+        <View style={{width:45, marginHorizontal: 10, justifyContent: 'center', alignItems: 'center'}}>
+        <FontAwesome name="search" size={40} color="white" />
+        </View>
         <TextInput style={{
           fontSize: 20, height: 40, width: '60%',
           backgroundColor: 'white', borderRadius: 5, borderWidth: 1
@@ -77,35 +50,36 @@ const ListChat = ({ navigation, route }) => {
         <TouchableOpacity onPress={() => navigation.navigate("ScanQR", route.params)}>
           <FontAwesome name="qrcode" size={35} color="white" />
         </TouchableOpacity>
+        <View style={{ marginRight: 10}}>
         <AntDesign name="adduser" size={35} color="white" />
+        </View>
       </View>
-      <View style={{ paddingHorizontal: 10 }}>
+      <View>
         <FlatList data={obj.conversation}
           renderItem={({ item }) => (
             item.user &&
-            <TouchableOpacity style={{ height: 70, flexDirection: 'row',
-             justifyContent: 'space-between', alignItems: 'center',
-            paddingVertical: 5}}
-              onPress={() => navigation.navigate("Chat",
-                id == 'jgfqCBTFdEgDmpHHXaNHdZV8B982' ?
-                  'RGpCgF0lR1aGVcttckhAbBHWcSp2' : 'jgfqCBTFdEgDmpHHXaNHdZV8B982'
-              )}
+            <TouchableOpacity style={{
+              height: 70, flexDirection: 'row', alignItems: 'center',
+              flex: 1
+            }}
+              onPress={() => navigation.navigate("Chat", item.user.id)}
             >
-              <View style={{ width: '15%', justifyContent: 'center', alignItems: 'center' }}>
-                <Image source={{ uri: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg' }} style={{ width: 50, height: 50, borderRadius: 25 }} />
+              <View style={{ width: 65, paddingHorizontal: 7, justifyContent: 'center', alignItems: 'center' }}>
+                <Image source={{ uri: item.user.avt }} style={{ width: 50, height: 50, borderRadius: 25 }} />
               </View>
-              <View style={{
-                width: '70%', paddingHorizontal: 10,
-                borderBottomWidth: 1, borderBottomColor: 'grey',
-                height: 70, justifyContent: 'center', alignItems: 'flex-start'
-              }}>
-                <Text style={{ fontSize: 20 }} numberOfLines={1}>{item.user.id}</Text>
-                <Text style={{ fontSize: 16, color: 'grey' }}>{item.lastMessage.content}</Text>
-              </View>
-              <View style={{ width: '15%', justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 16, color: 'grey' }}>4 giờ</Text>
-                <View style={{ backgroundColor: 'red', borderRadius: 10, justifyContent: 'center', alignItems: 'center', width: '50%' }}>
-                  <Text style={{ fontSize: 16, color: 'white' }}>1</Text>
+              <View style={{width: width - 65, flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: 'grey'}}>
+                <View style={{
+                  width: width - 145, paddingHorizontal: 10,
+                  height: 70, justifyContent: 'center'
+                }}>
+                  <Text style={{ fontSize: 20 }} numberOfLines={1}>{item.user.username}</Text>
+                  <Text style={{ fontSize: 16, color: 'grey' }}>{item.lastMessage.content}</Text>
+                </View>
+                <View style={{ width: 70, marginRight: 10, justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ fontSize: 14, color: 'grey' }}>{calcTime(item.lastMessage.senderDate)}</Text>
+                  <View style={{ backgroundColor: 'red', borderRadius: 10, justifyContent: 'center', alignItems: 'center', width: 30 }}>
+                    <Text style={{ fontSize: 16, color: 'white' }}>1</Text>
+                  </View>
                 </View>
               </View>
             </TouchableOpacity>

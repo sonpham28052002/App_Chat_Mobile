c169f89f63976767b237a25e9f80d824e96f0a2d
Nguyen Chi Cuong <20020331.cuong@student.iuh.edu.vn>
2024-04-05 13:28:30 +0700

    feat send file chat

--- screens/afterLogin/listChat/Chat.js
+++ screens/afterLogin/listChat/Chat.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, Dimensions, Animated, TouchableOpacity, Alert, KeyboardAvoidingView, Platform } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { Button,View,StyleSheet, Dimensions, Animated, TouchableOpacity, Alert, KeyboardAvoidingView, Platform,Text,Linking, Image} from 'react-native';
+import { GiftedChat, Message} from 'react-native-gifted-chat';
 import { TextInput } from 'react-native-paper';
 import { Entypo, FontAwesome, MaterialIcons } from '@expo/vector-icons';
 // import EmojiSelector, { Categories } from "react-native-emoji-selector";
@@ -8,8 +8,10 @@ import EmojiPicker from 'rn-emoji-keyboard'
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import { useSelector } from 'react-redux';
-import ImagePickerComponent from '../../../components/ImagePickerComponent'; // Import ImagePickerComponent
+import ImagePickerComponent from '../../../components/ImagePickerComponent';
+import FilePickerComponent from '../../../components/FilePickerComponent';
 import 'react-native-get-random-values';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const { v4: uuidv4 } = require('uuid');
 const Chat = ({ navigation, route }) => {
     console.log('Receive:', route.params);
@@ -17,6 +19,7 @@ const Chat = ({ navigation, route }) => {
     const [messages, setMessages] = useState([])
     var stompClient = useRef(null);
     const [uriImage, setUriImage] = useState(null);
+      const [uriFile, setUriFile] = useState(null);
     const textInputRef = useRef(null);
     const [position, setPosition] = useState({ start: 0, end: 0 });
     const [mess, setMess] = useState('');
@@ -25,6 +28,7 @@ const Chat = ({ navigation, route }) => {
     // const animate = useRef(new Animated.Value(height - 50)).current;
     // const [extend, setExtend] = useState(false);
     const [showEmoji, setShowEmoji] = useState(false);
+const [fileSelected, setFileSelected] = useState(false);
 
     useEffect(() => {
         navigation.setOptions({
@@ -72,9 +76,10 @@ const Chat = ({ navigation, route }) => {
         };
         if(message.content)
             newMessage.text = message.content;
-        else{
+        else if(message.url)
             newMessage.image = message.url;
-        }
+        else if(message.file)
+            newMessage.file = message.file;
         setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessage));
     }
 
@@ -92,25 +97,29 @@ const Chat = ({ navigation, route }) => {
             };
             if(mess && type === 'Text')
                 chatMessage.content = mess;
-            else{
+            else if (type === 'Image') {
                 const titleFile = uriImage.substring(uriImage.lastIndexOf("/") + 1);
                 chatMessage.size = 10;
                 chatMessage.titleFile = titleFile;
                 chatMessage.url = uriImage;
-                console.log("chatMes",chatMessage);
+            } else if (type === 'File') {
+                const titleFile = uriFile.substring(uriFile.lastIndexOf("/") + 1);
+                chatMessage.size = 10;
+                chatMessage.titleFile = titleFile;
+                chatMessage.url = uriFile;
             }
             stompClient.current.send("/app/private-single-message", {}, JSON.stringify(chatMessage));
         }
     }
 
-    const handleSend = () => {
-        if (mess.trim() === '' && !uriImage) {
-            Alert.alert("Chưa gửi tin nhắn hoặc chọn ảnh"); 
+     const handleSend = () => {
+        if (mess.trim() === '' && !uriImage && !uriFile) {
+            Alert.alert("Chưa gửi tin nhắn hoặc chọn ảnh hoặc file");
         } else {
             if (mess.trim() !== '') {
                 const id = uuidv4();
                 const newMessage = {
-                    _id: id, // Generate unique ID for the message
+                    _id: id,
                     text: mess.trim(),
                     createdAt: new Date(),
                     user: {
@@ -125,14 +134,21 @@ const Chat = ({ navigation, route }) => {
             } else if (uriImage) {
                 handleSendImage();
                 setUriImage(null);
+            } else if (uriFile) {
+                handleSendFile();
+                setUriFile(null);
             }
         }
     };
 
+
     const handleImageSelect = (uri) => {
         setUriImage(uri);
     };
-
+     const handleFileSelect = (uri) => {
+        setUriFile(uri);
+    };
+//Gửi hình ảnh
     const handleSendImage = () => {
         const id = uuidv4();
         // const id=new Date();
@@ -148,23 +164,98 @@ const Chat = ({ navigation, route }) => {
             },
         };
         const fileType = uriImage.substring(uriImage.lastIndexOf(".") + 1);
-        sendMessage(id, fileType)
+        sendMessage(id, "Image")
         setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessage));
     };
 
+const getFileExtension = (uri) => {
+    return uri.substring(uri.lastIndexOf(".") + 1);
+};
+
+const FileMessage = ({ currentMessage }) => {
+    const fileExtension = getFileExtension(currentMessage.file);
+    let iconName;
+
+    switch (fileExtension) {
+        case 'pdf':
+            iconName = 'file-pdf-box';
+            break;
+        case 'doc':
+        case 'docx':
+            iconName = 'file-word';
+            break;
+        case 'xls':
+        case 'xlsx':
+            iconName = 'file-excel';
+            break;
+        case 'ppt':
+        case 'pptx':
+            iconName = 'file-powerpoint';
+            break;
+        default:
+            iconName = 'file';
+    }
+
+    return (
+        <View style={styles.fileMessageContainer}>
+            <TouchableOpacity onPress={() => Linking.openURL(currentMessage.file)}>
+                <MaterialCommunityIcons name={iconName} size={50} color="#1E90FF" />
+            </TouchableOpacity>
+            {/* <Text style={{color:'#111111',fontSize:10}}>{currentMessage.file.substring(currentMessage.file.lastIndexOf("/") + 1)}</Text> */}
+            <Text style={{color:'#111111',fontSize:10}}>{new Date(currentMessage.createdAt).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}</Text>
+        </View>
+    );
+};
+    //Gửi file
+ const handleSendFile = () => {
+        if (uriFile) {
+            const id = uuidv4();
+            const newMessage = {
+                _id: id,
+                text: 'Mở file',
+                file: uriFile,
+                createdAt: new Date(),
+                user: {
+                    _id: sender.id,
+                    name: sender.userName,
+                    avatar: sender.avt,
+                },
+            };
+            sendMessage(id, 'File');
+            setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessage));
+            setFileSelected(true);
+        } else {
+            Alert.alert("Chọn file cần gửi.");
+        }
+    };
     const handleFocusText = () => {
         if (textInputRef.current) {
             textInputRef.current.focus();
         }
     };
-
+const renderMessage = (messageProps) => {
+    const { currentMessage } = messageProps;
+    if (currentMessage.file) {
+        return  <FileMessage currentMessage={currentMessage} />;
+        //  <Message {...messageProps} />;
+    } else if (currentMessage.text) { 
+        return (
+            <Message {...messageProps} />
+        );
+    } else if (currentMessage.image) { 
+        return (
+            <Message {...messageProps} />
+        );
+    }
+    return null;
+};
     return (
         <View style={{ width: width, flex: 1, height: height - 80, justifyContent: 'space-between' }}>
             <KeyboardAvoidingView style={{flex: 1}}
                 keyboardVerticalOffset={50}
                 behavior={Platform.OS == "ios"? "padding" : undefined}
             >
-            <View style={{ height: height - 80, backgroundColor: 'lightgray', marginBottom: 25 }}>
+            <View style={{ height: height - 110, backgroundColor: 'lightgray', marginBottom: 25 }}>
                 <GiftedChat
                     renderInputToolbar={(props) =>
                         <View style={{ flexDirection: 'row', width: width, backgroundColor: 'white', alignItems: 'center' }}>
@@ -193,6 +284,7 @@ const Chat = ({ navigation, route }) => {
                                     }}
                                 >
                                     <Entypo name="emoji-happy" size={35} color={colorEmoji} />
+                                   
                                 </TouchableOpacity>
                                 <View style={{ marginHorizontal: 10, width: width - 200 }}>
                                     <TextInput placeholder="Tin nhắn" style={{ backgroundColor: 'white', fontSize: 20, width: '100%' }}
@@ -206,7 +298,8 @@ const Chat = ({ navigation, route }) => {
                                     />
                                 </View>
                                 <View style={{ flexDirection: 'row', width: 85, justifyContent: 'space-between' }}>
-                                    <Entypo name="dots-three-horizontal" size={35} color="black" />
+                                    {/* <Entypo name="dots-three-horizontal" size={35} color="black" /> */}
+                                    <FilePickerComponent onSelectFile={handleFileSelect}/>
                                     <ImagePickerComponent onSelectImage={handleImageSelect} />
                                 </View>
                             </View>
@@ -225,6 +318,8 @@ const Chat = ({ navigation, route }) => {
                         name: sender.userName,
                         avatar: sender.avt
                     }}
+
+                      renderMessage={renderMessage}
                 />
             </View>
             </KeyboardAvoidingView>
@@ -252,5 +347,21 @@ const Chat = ({ navigation, route }) => {
         </View>
     );
 }
+const styles = StyleSheet.create({
+    fileMessageContainer: {
+        alignItems: 'center',
+        alignContent:'flex-end',
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        margin: 5,
+        width:'30%',
+        marginLeft: '75%',
+        //backgroundColor: '#1E90FF',
+    },
+    fileMessageText: {
+        fontSize: 16,
+        marginBottom: 10,
+    },
+});
 
 export default Chat;

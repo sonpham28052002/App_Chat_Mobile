48335ae0d51c709d366f9335e1f45a25080ac9ee
Sơn Nguyễn <20106421.son@student.iuh.edu.vn>
2024-04-22 19:37:43 +0700

    fix send message in a new group - just create

--- screens/afterLogin/listChat/ListChat.js
+++ screens/afterLogin/listChat/ListChat.js
@@ -38,14 +38,12 @@ const ListChat = ({ navigation }) => {
   // account reducer
   const id = useSelector((state) => state.account.id);
   const currentUser = useSelector((state) => state.account);
-  // const stateConversations = useSelector((state) => state.account.conversation);
   const [conversations, setConversations] = useState(currentUser.conversation);
-  let conversation = useRef({})
+  let conversationsRef = useRef(conversations);
 
   // message reducer
   const receiverId = useSelector((state) => state.message.id);
   let r = useRef('');
-  const messages = useSelector((state) => state.message.messages);
 
   const [selectedItem, setSelectedItem] = useState(null);
   const [deleteMode, setDeleteMode] = useState(false);
@@ -105,6 +103,10 @@ const ListChat = ({ navigation }) => {
   }, [currentUser.conversation]);
 
   useEffect(() => {
+    conversationsRef.current = conversations;
+  }, [conversations]);
+
+  useEffect(() => {
     if (!socketConnected) {
       const socket = new SockJS('https://deploybackend-production.up.railway.app/ws');
       stompClient.current = Stomp.over(socket);
@@ -118,9 +120,9 @@ const ListChat = ({ navigation }) => {
     stompClient.current.subscribe('/user/' + id + '/groupChat', onGroupMessageReceived)
     stompClient.current.subscribe('/user/' + id + '/retrieveMessage', onRetrieveMessage)
     stompClient.current.subscribe('/user/' + id + '/deleteMessage', onDeleteResult)
+    stompClient.current.subscribe('/user/' + id + '/createGroup', onCreateGroup)
 
     stompClient.current.subscribe('/user/' + id + '/deleteConversation', onReceiveDeleteConversationResponse);
-    stompClient.current.subscribe('/user/' + id + '/createGroup', onCreateGroup)
     stompClient.current.subscribe('/user/' + id + '/addMemberIntoGroup', onCreateGroup)
     stompClient.current.subscribe('/user/' + id + '/removeMemberInGroup', onCreateGroup)
     stompClient.current.subscribe('/user/' + id + '/outGroup', onCreateGroup)
@@ -128,8 +130,9 @@ const ListChat = ({ navigation }) => {
     // stompClient.current.subscribe('/user/' + id + '/deleteMessage', onReceiveFromSocket)
   }
 
-  const onCreateGroup = (message) => {
-    updateMess();
+  const onCreateGroup = (payload) => {
+    const conversation = JSON.parse(payload.body);
+    dispatch(addLastConversation(conversation));
   }
 
   const checkIsChatting = (senderId, messageReceiverId) => {
@@ -149,7 +152,6 @@ const ListChat = ({ navigation }) => {
     let userId = message.sender.id == id ? message.receiver.id : message.sender.id;
     let index = conversations.findIndex(conv => conv.user && conv.user.id === userId);
     //update message in listchat
-    console.log('index:', index);
     if( index === -1 ){
       getConversation(id).then(conv => {
         dispatch(addLastConversation(conv));
@@ -169,7 +171,7 @@ const ListChat = ({ navigation }) => {
       if (checkIsChatting(message.sender.id, message.receiver.id)) {
         let newMess = onMessageReceive(message,
           { id: currentUser.id, userName: currentUser.userName, avt: currentUser.avt },
-          conversation.current.user)
+          conversations[index].user)
         if (newMess)
           dispatch(addMess(newMess))
       }
@@ -179,17 +181,14 @@ const ListChat = ({ navigation }) => {
   const onGroupMessageReceived = (payload) => {
     const message = JSON.parse(payload.body);
     let idGroup = message.receiver.id.split('_')[1];
-    let index = 0
-    while (index < conversations.length && conversations[index].idGroup !== idGroup) {
-      index++;
-    }
+    let index = conversationsRef.current.findIndex(conv => conv.idGroup === idGroup);
     dispatch(addLastMessage({ message: message, index: index }));
 
     // kiểm tra xem tin nhắn nhận được có phải tin nhắn trong group đang chat hay không
     if (checkIsChatting(message.sender.id, message.receiver.id)){
       let newMess = onMessageReceive(message,
         { id: currentUser.id, userName: currentUser.userName, avt: currentUser.avt },
-        { id: idGroup, members: conversations[index].members })
+        { id: idGroup, members: conversationsRef.current[index].members })
       if (newMess)
         dispatch(addMess(newMess))
     }
